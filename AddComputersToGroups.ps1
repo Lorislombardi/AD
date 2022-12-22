97361329fddaa8dce80c4d5d945402be3e6b299b
Lorislombardi <69912688+Lorislombardi@users.noreply.github.com>
2022-12-23 00:01:34 +0100

    Update AddComputersToGroups.ps1
    version optimisé

--- AddComputersToGroups.ps1
+++ AddComputersToGroups.ps1
@@ -1,36 +1,35 @@
-﻿#Variable used for the script
-$GroupName="CN=G_Test_Script,OU=AD,OU=Groups,OU=Formation,DC=Formation,DC=local"
-$OUName="OU=Workstation,OU=Formation,DC=Formation,DC=local"
-$CSVFile='C:\Users\Administrator\Desktop\Computer.csv'
-$LogFile='C:\Temp\LogAddComputers.txt'
+#Variable used for the script
+$GroupName = "CN=G_Test_Script,OU=AD,OU=Groups,OU=Formation,DC=Formation,DC=local"
+$OUName = "OU=Workstation,OU=Formation,DC=Formation,DC=local"
+$CSVFile = 'C:\Users\Administrator\Desktop\Computer.csv'
+$LogFile = 'C:\Temp\LogAddComputers.txt'
 
-#List all objet in the OU and create log file
-$ordiAD=Get-ADObject -Filter * -SearchBase $OUName | Select-Object name, DistinguishedName
-$ComputeraddedGroups=""
+#List all objects in the OU and create log file
+$ordiAD = Get-ADObject -Filter * -SearchBase $OUName | Select-Object Name, DistinguishedName
+
+# Add computers in the OU to the group and create log file
 Foreach ($ordAD in $ordiAD) 
 {
-    Get-ADComputer $ordAD.Name -Properties MemberOf | %{if ($_.MemberOf -like $GroupName) {$ComputeraddedGroups="1"} }
-    If ($ComputeraddedGroups -ne 1)
+    # Use the -Server parameter to specify the domain controller to use
+    # This can help improve performance if you are querying a remote domain controller
+    $memberOf = Get-ADComputer $ordAD.Name -Properties MemberOf -Server <domain controller name> | Select-Object -ExpandProperty MemberOf
+    if ($memberOf -notcontains $GroupName)
     {
-        add-ADGroupMember -Identity $GroupName -Members $ordAD.DistinguishedName
-        $Message="The computer" + $ordAD.Name + " is added to the group the $(get-date)" 
-        Add-Content -Path $LogFile $Message
-        
+        Add-ADGroupMember -Identity $GroupName -Members $ordAD.DistinguishedName
+        $Message = "The computer $($ordAD.Name) is added to the group on $(Get-Date)" 
+        Add-Content -Path $LogFile -Value $Message
     }
-    $ComputeraddedGroups=""
-     
 }
 
-#Add computers in the csv file and create log file
+# Add computers in the CSV file and create log file
 #Example for the CSV file : 
-   #Name;DistinguishedName
-   #CLW10;CN=CLW10,OU=Hybridation,OU=Workstation,OU=Formation,DC=Formation,DC=local
-   
-$ComputerCSV=Import-CSV -Path $CSVFile -Delimiter ";"
-Foreach ($ordAD in $ordiAD) 
+#   Name;DistinguishedName
+#   CLW10;CN=CLW10,OU=Hybridation,OU=Workstation,OU=Formation,DC=Formation,DC=local
+
+$ComputerCSV = Import-CSV -Path $CSVFile -Delimiter ";"
+Foreach ($computer in $ComputerCSV) 
 {
-    add-ADGroupMember -Identity $GroupName -Members $ordAD.DistinguishedName
-    echo $ordAD.name
-    echo $ordAD.DistinguishedName
-     
+    Add-ADGroupMember -Identity $GroupName -Members $computer.DistinguishedName
+    $Message = "The computer $($computer.Name) is added to the group on $(Get-Date)" 
+    Add-Content -Path $LogFile -Value $Message
 }
